b8eecce85507f4f4aa5269f520bb12f242ca0f1d
riro114 <74780895+riro114@users.noreply.github.com>
2020-11-20 15:55:55 +0000

    lesson.js

---
+++ lessons.js
@@ -0,0 +1,110 @@
+
+//array of lesson objects
+lessons = [
+{
+
+    id: "0",
+    tiitle : "Math" ,
+    location : "london",
+    price : "100",
+    spaces : "5",
+    img : "Math.png",
+},
+
+{
+    id: "1",
+    tiitle : "History" ,
+    location : "portsmouth",
+    price : "110",
+    spaces : "5",
+    img: "history.png",
+},
+{
+    id: "2",
+    tiitle: "English" ,
+    location : "kent",
+    price : "120",
+    spaces : "5",
+    img: "english.png",
+
+},
+{
+    id: "3", 
+    tiitle: "math" ,
+    location: "york",
+    price : "130",
+    spaces : "5",
+    img: "Math.png",
+
+
+},
+{
+
+    id: "4",
+    tiitle : "Science" ,
+    location : "york",
+    price : "140",
+    spaces : "5",
+    img: "science.png",
+},
+
+{
+
+    id: "5",
+    tiitle : "Geography" ,
+    location : "brighton",
+    price : "150",
+    spaces : "5",
+    img: "geography.png",
+
+},
+{
+
+    id: "6",
+    tiitle : "Art" ,
+    location : "london",
+    price : "160",
+    spaces : "5",
+    img: "Art.png",
+},
+{
+
+    id: "7",
+    tiitle : "Music" ,
+    location : "Surry",
+    price : "170",
+    spaces : "5",
+    img: "music.png",
+},
+{
+
+    id: "8",
+    tiitle : "Computing" ,
+    location : "Bath",
+    price : "180",
+    spaces : "5",
+    img: "computing.png",
+},
+{
+
+    id: "9",
+    tiitle : "Design" ,
+    location : "leeds",
+    price : "190",
+    spaces : "5",
+    img: "design.png",
+},
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+]
